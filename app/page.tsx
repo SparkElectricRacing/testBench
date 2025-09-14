8aa7708dec715a930cc96e439cfbd43d5b60c74d
Animesh Bhatnagar <44537017+Ani-mation@users.noreply.github.com>
2025-09-14 16:20:56 -0400

    Initial commit
    Created using default template

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <body>
+      <h1>Hello SPARK!</h1>
+      <Image src="/sparklogovector.svg" alt="SPARK Logo" width="200" height="200"/>
+    </body>
+  );
+}
